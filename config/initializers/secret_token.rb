06274ade38388e5b912516519dcc2e9898a3db7f
azmdar <azm.dar@gmail.com>
2013-12-18 18:43:11 +0100

    onemonthrails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pinteresting::Application.config.secret_key_base = 'af022afa580e66e9697a42d60f4d53e39601044dcdd8bff580121782c7c4d6e2fe59ba320df12c4c9e1b7de52c81daa4868eaf30daaafc38f985a12820830317'
