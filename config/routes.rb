9bc876e3c17947c8dc44e1cf7cf8f3897c0fe940
azmdar <azm.dar@gmail.com>
2013-12-19 19:38:12 +0100

    created home and about pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Pinteresting::Application.routes.draw do
+  root "pages#home"
+  get "about" =>"pages#about"
+  get "contactus" =>"pages#contactus"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
