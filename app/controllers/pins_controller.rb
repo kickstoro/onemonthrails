df4800aeaf4c56430dc5f3c01803a842d01ea863
azmdar <azm.dar@gmail.com>
2013-12-22 02:43:12 +0100

    add image upload with paperclip

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -2,7 +2,7 @@ class PinsController < ApplicationController
   before_action :set_pin, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!, except: [:index, :show]
   def index
-    @pins = Pin.to_a
+    @pins = Pin.all
   end
 
   def show
@@ -43,13 +43,15 @@ class PinsController < ApplicationController
       @pin = Pin.find(params[:id])
     end
 
+    def pin_params
+      params.require(:pin).permit(:description, :image)
+    end
+
     def correct_user
       @pin = current_user.pins.find_by(id: params[:id])
       redirect_to pins_path, notice: "Not authorized to edit this pin" if @pin.nil?
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
-    def pin_params
-      params.require(:pin).permit(:description, :image)
-    end
+    
 end
